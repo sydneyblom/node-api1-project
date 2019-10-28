f386b35c40f24e5f02a49c1be25db583b8bbd0f0
Sydney Blom <sydneyblom@#gmail.com>
2019-10-28 15:50:37 -0500

    added delete

--- index.js
+++ index.js
@@ -73,7 +73,21 @@ server.post ('/users', (req,res) =>{
         }
     });
     
-
+server.delete('/users/:id', (req, res)=>{
+const id =req.params.id;
+db.remove(id)
+.then(users => {
+    if(users) {
+res.status(200).json(users);
+    }
+    else {
+        res.status(404).json({message: 'The user with the specified ID does not exist.'});
+    }
+})
+.catch(error=>{
+    res.status(500).json({error: 'The user could not be removed.'})
+})
+})
 
 
 
