19df661d9dd9867c4aa6f47a33a7cf7da721f430
Sydney Blom <sydneyblom@#gmail.com>
2019-10-28 14:05:38 -0500

    post req

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // implement your API here
+// import express from 'express'; // ES Modules
+const express = require('express');
+
+const server = express(); //creates a server
+
+//gets access to DB
+const db = require('./data/db');
+
+//middleware - teaches how to read JSON needed for post and put to work
+server.use(express.json());
+
+//configures server for get req
+server.get('/',(req, res) =>{
+    res.send("Server is working")
+})
+
